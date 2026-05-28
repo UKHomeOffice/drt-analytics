d2bd02aa8f0479ded9e61eccf5f002d07e8198a6
slawekb <259381811+slawomirbedraHO@users.noreply.github.com>
2026-05-28 16:18:51 +0100

    DRTII-2184 | Add SBT configuration files and update dependencies.

---
+++ project/AppDependencies.scala
@@ -0,0 +1,42 @@
+import sbt.*
+
+object AppDependencies {
+  private val pekkoVersion = "1.4.0"
+  private val sparkVersion = "4.1.1"
+  private val netlibVersion = "3.0.3"
+
+  val compileDependencies: Seq[ModuleID] = Seq(
+    "org.apache.pekko" %% "pekko-slf4j" % pekkoVersion,
+    "ch.qos.logback" % "logback-classic" % "1.5.33",
+    "ch.qos.logback.contrib" % "logback-json-classic" % "0.1.5",
+    "ch.qos.logback.contrib" % "logback-jackson" % "0.1.5",
+    "org.apache.spark" %% "spark-mllib" % sparkVersion,
+    "org.apache.spark" %% "spark-sql" % sparkVersion,
+    "org.scala-lang.modules" %% "scala-xml" % "2.4.0",
+    "org.apache.pekko" %% "pekko-http" % "1.3.0",
+    "org.apache.pekko" %% "pekko-persistence" % pekkoVersion,
+    "org.apache.pekko" %% "pekko-persistence-query" % pekkoVersion,
+    "org.apache.pekko" %% "pekko-stream" % pekkoVersion,
+    "org.apache.pekko" %% "pekko-pki" % pekkoVersion,
+    "org.apache.pekko" %% "pekko-persistence-jdbc" % "1.2.0",
+    "org.postgresql" % "postgresql" % "42.7.11",
+    "joda-time" % "joda-time" % "2.14.2",
+    "uk.gov.homeoffice" %% "drt-lib" % "vDEV",
+    "org.typelevel" %% "cats-core" % "2.13.0",
+    "software.amazon.awssdk" % "s3" % "2.30.38",
+    "com.typesafe" %% "ssl-config-core" % "0.7.1",
+    "dev.ludovic.netlib" % "blas" % netlibVersion,
+    "dev.ludovic.netlib" % "lapack" % netlibVersion,
+    "dev.ludovic.netlib" % "arpack" % netlibVersion
+  )
+
+  val testDependencies: Seq[ModuleID] = Seq(
+    "org.scalatest" %% "scalatest" % "3.2.20" % Test,
+    "org.specs2" %% "specs2-core" % "4.23.0" % Test,
+    "org.apache.pekko" %% "pekko-testkit" % pekkoVersion % Test,
+    "org.apache.pekko" %% "pekko-stream-testkit" % pekkoVersion % Test,
+    "org.apache.pekko" %% "pekko-persistence-testkit" % pekkoVersion % Test
+  )
+
+  val all: Seq[ModuleID] = compileDependencies ++ testDependencies
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,12 +1,14 @@
-
 ThisBuild / libraryDependencySchemes ++= Seq(
   "org.scala-lang.modules" % "scala-xml" % VersionScheme.Always
 )
 
-addSbtPlugin("com.github.sbt"         % "sbt-native-packager"   % "1.11.7")
-addSbtPlugin("org.scoverage"          % "sbt-scoverage"         % "2.4.4")
-addSbtPlugin("org.johnnei.scapegoat" %% "sbt-scapegoat"         % "1.3.7")
-addSbtPlugin("com.eed3si9n"           % "sbt-assembly"          % "2.3.1")
-addSbtPlugin("net.nmoncho"            % "sbt-dependency-check"  % "1.8.4")
-addSbtPlugin("com.timushev.sbt"       % "sbt-updates"           % "0.6.3")
+addSbtPlugin("com.github.sbt" % "sbt-native-packager" % "1.11.7")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.6.1")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.4.4")
+addSbtPlugin("org.wartremover" % "sbt-wartremover" % "3.5.7")
+addSbtPlugin("org.johnnei.scapegoat" %% "sbt-scapegoat" % "1.3.7")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "2.3.1")
+addSbtPlugin("net.nmoncho" % "sbt-dependency-check" % "1.9.0")
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.6.4")
+
 addDependencyTreePlugin
